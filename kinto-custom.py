d6e233b1a8ecf3d840228684b30adb3937b40ed9
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2020-08-11 08:49:19 +0200

    feat(kinto): fix browser navigation shortcuts

--- kinto-custom.py
+++ kinto-custom.py
@@ -1,7 +1,7 @@
 ##### Custom bindings
 # Keybindings for Browsers
 define_keymap(re.compile(browserStr, re.IGNORECASE),{
-    K("RC-Left"): K("M-Left"),     # Navigate back
-    K("RC-Right"): K("M-Right"),   # Navigate forward 
+    K("RC-Alt-Left"): K("M-Left"),     # Navigate back
+    K("RC-Alt-Right"): K("M-Right"),   # Navigate forward 
     K("RC-y"): K("LC-h"),          # Show history
 })
